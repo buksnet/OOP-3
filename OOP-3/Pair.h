d32a5e6a9a0e3eb9b50d2974b160b38a54b4704f
buksnet <bukovskii.den@gmail.com>
2024-06-07 18:22:52 +0500

    add report.docx

--- OOP-3/Pair.h
+++ OOP-3/Pair.h
@@ -1,10 +1,23 @@
 #pragma once
 #include <iostream>
 
-class Pair {
-private:
+class Base {
+protected:
 	int first;
 	double second;
+public:
+	virtual double sub() = 0;
+	virtual void add(int) = 0;
+	virtual void add(double) = 0;
+
+	virtual void setFirst(int) = 0;
+	virtual void setSecond(double) = 0;
+
+	virtual int getFirst() = 0;
+	virtual double getSecond() = 0;
+};
+
+class Pair: public Base {
 public:
 
 	Pair();
@@ -12,17 +25,17 @@ public:
 	Pair(Pair& T);
 	~Pair();
 
-	double sub();
-	void add(int thing);
-	void add(double thing);
+	double sub() override;
+	void add(int thing) override;
+	void add(double thing) override;
 
-	void setFirst(int first);
+	void setFirst(int first) override;
 
-	void setSecond(double second);
+	void setSecond(double second) override;
 
-	int getFirst();
+	int getFirst() override;
 
-	double getSecond();
+	double getSecond() override;
 
 	void operator= (Pair& T);
 
